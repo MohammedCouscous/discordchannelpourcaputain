13ded42a499b105c558b1d841dfd7ec42b38b538
MohammedCouscous <33389063+MohammedCouscous@users.noreply.github.com>
2019-01-06 18:19:02 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,326 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+Object.defineProperty(this, 'client', { value: client });
+
+// INITIALISATION DU BOT
+client.on('ready', () => {
+    console.log(`Attend fils de pute je démarre.`);
+    console.log(`Attend fils de pute je démarre..`);
+    console.log(`Attend fils de pue je démarre...`);
+    console.log(`J'suis prêt enculé`);
+    console.log(`Oublie pas de send nudes sur mon Snap : moamedcouscous6`)
+});
+
+// JEUX A QUOI LE BOT JOUE
+client.on('ready', () => {
+    client.user.setGame('Strike Viktor')
+});
+
+
+phrase = "Y'a une channel pour ça PUTAIN sorry caps"; // INITILISATION DES VARIABLES
+
+// PROGRAMME INTERNE 
+client.on('message', msg => {
+  if (msg.content === 't!help') {
+      msg.reply(phrase);
+      msg.delete(1);
+
+    }
+  if (msg.content === 't!invite') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!leveledroles') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!ping') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!selfroles') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!support') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!background') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!badges') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!dailies') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!played') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!profile') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!rank') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!reputation') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!rep') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!setinfo') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!settitle') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!top') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+  if (msg.content === 't!8ball') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!cah') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!cat') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!catfacts') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!choose') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!coin') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!cookie') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!dice') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!dog') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!dogfacts') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!fishy') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!fortune') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!numberfacts') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!psychopass') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!ratewaifu') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!reverse') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!rps') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!slots') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!tags') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!tatsugotchi') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!credits') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!points') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!color') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!converter') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!crypto') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!google') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!image') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!lotto') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!math') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!remindme') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!shorten') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!stocks') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!strawpoll') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!todo') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!urban') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!vote') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!weather') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!wiki') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!youtube') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!apikey') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!avatar') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!botlist') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!changelog') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!channel') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!info') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!role') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!server') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!shared') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!usage') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!anime') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!manga') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!osu') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!sic') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!beautiful') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    if (msg.content === 't!hackerman') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+    // AU DESSUS, MESSAGE QUI INTERDIT LE BOT TATSUMAKI
+    if (msg.content === '<@119482224713269248>') {
+        msg.reply(phrase);
+        msg.delete(1);
+    }
+	
+        if (msg.content === '.') {
+            msg.reply(phrase);
+            msg.delete(1);
+        }
+	
+});
+
+client.login("NTMwNTk5MjQ4MTU3NDc0ODI3.DxBuvg.VM3kpCAJowLW-JuyLjgfg953e1s")
